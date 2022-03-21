5f2d32e293493b31bbd9eac0f177762b48ded90a
kirill petrov <fantaskiras@gmail.com>
2022-03-21 18:05:22 +0300

    add JSON Server

--- src/Home.js
+++ src/Home.js
@@ -1,20 +1,30 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import BlogList from './BlogList';
 
 const Home = () => {
-  const [blogs, setBlogs] = useState([
-    { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
-    { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
-    { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 },
-  ]);
+  const [blogs, setBlogs] = useState(null);
+
+  useEffect(() => {
+    console.log('useEffect run');
+    fetch('http://localhost:8000/blogs')
+      .then(res => res.json())
+      .then(data => setBlogs(data));
+  }, []);
 
   return (
     <div className='home'>
-      <BlogList blogs={blogs} title='All Blogs' />
-      <BlogList
+      {
+        blogs &&
+        <BlogList
+          blogs={blogs}
+          title='All Blogs'
+        />
+      }
+      {/* 
+      <BlogList ist
         blogs={blogs.filter((blog) => blog.author === 'mario')}
         title="Mario's Blogs"
-      />
+      /> */}
     </div>
   );
 };
