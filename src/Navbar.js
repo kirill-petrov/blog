84cb9fcca0a1ef28c023514397fcc758dd9a0bd1
kirill petrov <fantaskiras@gmail.com>
2022-03-21 09:32:19 +0300

    useState

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,16 +1,18 @@
 function Navbar() {
   return (
     <nav className='navbar'>
-      <h1>The Dojo Blog</h1>
+      <h1>The Blog</h1>
       <div className='links'>
         <a href='/'>Home</a>
         <a
           href='/create'
-          style={{
-            color: 'white',
-            backgroundColor: '#f1356d',
-            borderRadius: '8px',
-          }}
+          // style={
+          //   {
+          //     color: 'white',
+          //     backgroundColor: '#f1356d',
+          //     borderRadius: '8px',
+          //   }
+          // }
         >
           New Blog
         </a>
