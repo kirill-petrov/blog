a9cd93a69ab0f11211718cc732d09dfb4cac6d7c
kirill petrov <fantaskiras@gmail.com>
2022-03-22 20:24:45 +0300

    404

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Navbar from './Navbar';
 import Home from './Home';
 import Create from './Create';
 import BlogDetails from './BlogDetails';
+import NotFound from './NotFound';
 
 function App() {
   return (
@@ -19,6 +20,7 @@ function App() {
             <Route path="/" element={<Home />} />
             <Route path="/create" element={<Create />} />
             <Route path="/blogs/:id" element={<BlogDetails />} />
+            <Route path="*" element={<NotFound />} />
 
           </Routes>
         </div>
--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -1,9 +1,19 @@
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 import useFetch from './useFetch';
 
 const BlogDetails = () => {
   const { id } = useParams();
   const { data: blog, error, isPending } = useFetch(`http://localhost:8000/blogs/${id}`);
+  const navigate = useNavigate();
+
+  const handleDelete = (id) => {
+    fetch(`http://localhost:8000/blogs/${id}`, {
+      method: 'DELETE',
+    })
+      .then(() => {
+        navigate('/');
+      });
+  };
 
   return (
     <div className="blog-details">
@@ -14,6 +24,9 @@ const BlogDetails = () => {
           <h2>{blog.title}</h2>
           <p>Written by <b>{blog.author}</b></p>
           <div>{blog.body}</div>
+          <button onClick={() => handleDelete(blog.id)}>
+            Delete
+          </button>
         </article>
       )}
 
